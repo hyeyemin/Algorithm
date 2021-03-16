f8f2a9e463eab90616b295f2b0016dd8b8cf3d21
hyeyemin <32701768+hyeyemin@users.noreply.github.com>
2021-03-16 09:59:10 +0900

    백준 14425

---
+++ Baekjoon/BJ_14425_문자열집합.java
@@ -0,0 +1,26 @@
+package week01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class BJ_14425_문자열집합 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		for(int i = 0; i < N; i++) {
+			map.put(br.readLine(), 0);
+		}
+		int result = 0;
+		for(int i = 0; i < M; i++) {
+			if(map.containsKey(br.readLine())) result++;
+		}
+		System.out.println(result);
+	}
+}
