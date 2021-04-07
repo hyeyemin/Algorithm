1bc1a0f2d3a7ae3b2a4254d3ce7e784fa48db0e5
hyeyemin <32701768+hyeyemin@users.noreply.github.com>
2021-04-07 23:47:21 +0900

    백준 1753

--- Baekjoon/BJ_1753_최단경로.java
+++ Baekjoon/BJ_1753_최단경로.java
@@ -1,4 +1,4 @@
-package com.ssafy.day18;
+package week03;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,27 +10,28 @@ import java.util.StringTokenizer;
 
 public class BJ_1753_최단경로 {
 	static class Node implements Comparable<Node>{
-		int v;
-		int w;
-		Node(int v, int w){
-			this.v = v;
-			this.w = w;
+		int vertex;
+		int cost;
+		
+		Node(int vertex, int cost){
+			this.vertex = vertex;
+			this.cost = cost;
 		}
+
 		@Override
 		public int compareTo(Node o) {
-			return this.w - o.w;
+			return this.cost - o.cost;
 		}
 		
 	}
-	static int V, E, K;
-	private static ArrayList<Node>[] list;
-	private static int[] dist;
+	static ArrayList<Node>[] list;
+	static int[] dist;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(br.readLine());
+		int V = Integer.parseInt(st.nextToken());
+		int E = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(br.readLine());
 		list = new ArrayList[V+1];
 		dist = new int[V+1];
 		Arrays.fill(dist, Integer.MAX_VALUE);
@@ -39,34 +40,30 @@ public class BJ_1753_최단경로 {
 		}
 		for(int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			list[u].add(new Node(v, w));
+			list[Integer.parseInt(st.nextToken())].add(new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
 		}
 		dijkstra(K);
 		for(int i = 1; i <= V; i++) {
 			if(dist[i] == Integer.MAX_VALUE) System.out.println("INF");
-			else System.out.println(dist[i]);
+			System.out.println(dist[i]);
 		}
 	}
-	private static void dijkstra(int start) {
+	private static void dijkstra(int k) {
 		PriorityQueue<Node> queue = new PriorityQueue<>();
-		boolean[] visit = new boolean[V+1];
-		queue.add(new Node(start, 0));
-		dist[start] = 0;
+		boolean[] visit = new boolean[dist.length];
+		queue.add(new Node(k, 0));
+		dist[k] = 0;
 		
-		while(!queue.isEmpty()){
+		while(!queue.isEmpty()) {
 			Node current = queue.poll();
-			int curV = current.v;
+			int v = current.vertex;
+			if(visit[v]) continue;
+			visit[v] = true;
 			
-			if(visit[curV]) continue;
-			visit[curV] = true;
-			
-			for(int i = 0; i < list[curV].size(); i++) {
-				if(dist[list[curV].get(i).v] > dist[curV] + list[curV].get(i).w) {
-					dist[list[curV].get(i).v] = dist[curV] + list[curV].get(i).w;
-					queue.add(new Node(list[curV].get(i).v, dist[list[curV].get(i).v]));
+			for(int i = 0; i < list[v].size(); i++) {
+				if(dist[list[v].get(i).vertex] > dist[v] + list[v].get(i).cost) {
+					dist[list[v].get(i).cost] = dist[v] + list[v].get(i).cost;
+					queue.add(new Node(list[v].get(i).vertex, dist[list[v].get(i).vertex]));
 				}
 			}
 		}
