461647ff23673ec93f587e49cab85aca4fe6f8da
hyeyemin <32701768+hyeyemin@users.noreply.github.com>
2021-05-07 21:49:52 +0900

    Add files via upload

--- src/programmers/Test01.java
+++ src/programmers/Test01.java
@@ -1,35 +1,24 @@
-package kakao;
-
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-
+package scf;
+import java.io.*;
 public class Test01 {
-	public static void main(String[] args) {
-		int[] gift_cards = new int[100000];
-		int[] wants = new int[100000];
-		Arrays.fill(gift_cards, 1);
-		Arrays.fill(wants, 2);
-		System.out.println(solution(gift_cards, wants));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		String[][] time = new String[n][2];
+		String start = "0000";
+		String end = "2359";
+		for(int i = 0; i < n; i++) {
+			String[] temp = br.readLine().replace(":", "").split(" ~ ");
+			time[i][0] = temp[0];
+			if(Integer.parseInt(start) < Integer.parseInt(time[i][0])) {
+				start = time[i][0];
+			}
+			time[i][1] = temp[1];
+			if(Integer.parseInt(end) > Integer.parseInt(time[i][1])) {
+				end = time[i][1];
+			}
+		}
+		if(Integer.parseInt(start) > Integer.parseInt(end)) System.out.println(-1);
+		else System.out.println(start.substring(0,2) + ":" + start.substring(2,4) + " ~ " + end.substring(0,2) + ":" + end.substring(2,4));
 	}
-	 public static int solution(int[] gift_cards, int[] wants) {
-	     int answer = gift_cards.length;
-	     Queue<Integer> q = new LinkedList<>();
-	     for(int i = 0; i < wants.length; i++) {
-	    	 q.offer(wants[i]);
-	     }
-	     for(int i = 0; i < gift_cards.length; i++) {
-	    	 int cnt = 0;
-	    	while(!q.isEmpty() && cnt != wants.length) {
-	    		cnt++;
-	    		int temp = q.poll();
-	    		if(temp == gift_cards[i]) {
-	    			answer--;
-	    			break;
-	    		}
-	    		else q.add(temp);
-	    	}
-	     }
-	     return answer;
-	 }
 }
